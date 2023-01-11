60d6714fde1eef3e8016e2a3ca8579ee52c58e2a
Mario Martinez <mariomttz@comunidad.unam.mx>
2023-01-10 18:40:23 -0600

    Autosave and autopairs configuration files added in the plugins directory

---
+++ lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,17 @@
+--[[
+               _                        _                _               
+  __ _  _   _ | |_  ___   _ __    __ _ (_) _ __  ___    | | _   _   __ _ 
+ / _` || | | || __|/ _ \ | '_ \  / _` || || '__|/ __|   | || | | | / _` |
+| (_| || |_| || |_| (_) || |_) || (_| || || |   \__ \ _ | || |_| || (_| |
+ \__,_| \__,_| \__|\___/ | .__/  \__,_||_||_|   |___/(_)|_| \__,_| \__,_|
+                         |_|                                             
+
+For more information, please visit: https://github.com/windwp/nvim-autopairs
+
+--]]
+
+-- Options configurations (Neovim API aliases).
+
+-- Neovim autopairs configuration.
+
+require("nvim-autopairs").setup({})
