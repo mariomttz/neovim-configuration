16f4cb580d2a288212c2ab4a06f776b9a89eb35d
Mario Martinez <mariomttz@comunidad.unam.mx>
2023-01-26 21:13:51 -0600

    The new configuration of the dashboard uploaded

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -13,22 +13,70 @@ For more information, please visit: https://github.com/glepnir/dashboard-nvim
 
 -- Options configurations (Neovim API aliases).
 
--- Dashboard options configurations.
-
-local db = require("dashboard")
-
 -- Dashboard configuration.
 
-db.custom_header = {
-    '',
-    '',
-    '███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗',
-    '████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║',
-    '██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║',
-    '██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║',
-    '██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║',
-    '╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝',
-    '                                                  ',
-    '[ Welcome mariomttz ]',
-    '',
-}
+require("dashboard").setup({
+    config = {
+        theme = "hyper",                                                -- Theme used for the configuration.
+
+        header = {                                                      -- Header configuration.
+                '',
+                '',
+                '',
+                '███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗',
+                '████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║',
+                '██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║',
+                '██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║',
+                '██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║',
+                '╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝',
+                '                                                  ',
+                '                                                  ',
+                '[ Welcome mariomttz ]',
+                '',
+                '',
+        },
+
+        shortcut = {
+            {
+                desc = "  New file",
+                group = "@property",
+                action = "",
+                key = "n",
+            },
+
+            {
+                desc = "  File browser",
+                group = "@property",
+                action = "NvimTreeToggle",
+                key = "b",
+            },
+
+            {
+                desc = "  Packer update",
+                group = "@property",
+                action = "PackerUpdate",
+                key = "u",
+            },
+        },
+
+        packages = {                                                    -- Show how many plugins neovim loaded.
+            enable = true
+        },
+
+        project = {                                                     -- Limit how many projects list, action when you press key or enter it will run this action.
+            limit = 5,
+            action = 'Telescope find_files cwd='
+        },
+
+        mru = {                                                         -- How many recently opened files displayed in the list.
+            limit = 10
+        },
+
+        footer = {                                                      -- Footer configuration.
+            "",
+            "",
+            "",
+            "Have a nice time mariomttz while using neovim!!!"
+        }   
+    }
+})
